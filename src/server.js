3e31f5541715a2e18a5566a6fa5e68852d497fc5
CyRa89 <90359416+CyRa89@users.noreply.github.com>
2021-09-14 14:56:58 -0500

    cambio de texto plano

--- src/server.js
+++ src/server.js
@@ -3,4 +3,12 @@ const http = require("http");
 const host ="localhost";
 const port = 3000;
 
-const server = http.createServer()
+const server = http.createServer(function(req, res){
+    res.statusCode = 200;
+    res.setHeader("Content-Type","text/plain");
+    res.end("Hello World!.");
+});
+
+server.listen(port, host, function(){
+    console.log("Si jala!!!");
+});
