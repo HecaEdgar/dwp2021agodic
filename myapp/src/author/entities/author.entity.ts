7efd943c0a77931a273ebb300e2cd8168e96cea5
Roberto Lopez <jrlopez233@gmail.com>
2021-10-15 14:51:25 -0500

    Se crean las entidades para author y relacion

--- myapp/src/author/entities/author.entity.ts
+++ myapp/src/author/entities/author.entity.ts
@@ -1,1 +1,17 @@
-export class Author {}
+import { AuthorBook } from 'src/author-book/entities/author-book.entity';
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+
+@Entity("authors")
+export class Author {
+    @PrimaryGeneratedColumn()
+    idauthors: number;
+
+    @Column()
+    name: string;
+
+    @Column({ default: '' })
+    origen?: string;
+
+    @OneToMany(type => AuthorBook, relacion => relacion.author)
+    book_author: AuthorBook[]
+}
